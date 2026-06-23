5e05ce0833d54c0b6748c6263f52bdfb653a0289
LAPTOP-RXI\RXI <lrxinn@qq.com>
2026-06-24 02:23:02 +0800

    Fix: add UNICODE defines for MinGW compilation

--- gui.cpp
+++ gui.cpp
@@ -8,6 +8,12 @@
 #ifndef NOMINMAX
 #define NOMINMAX
 #endif
+#ifndef UNICODE
+#define UNICODE
+#endif
+#ifndef _UNICODE
+#define _UNICODE
+#endif
 #ifndef _WIN32_WINNT
 #define _WIN32_WINNT 0x0601
 #endif
